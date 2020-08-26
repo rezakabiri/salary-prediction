a78656ec016aef6fb27f78a20ae5e49b9f09db85
rezakabiri <mmr.kabiri@gmail.com>
2020-08-26 15:47:23 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Simple portfolio project to preodict the salaries based on given attributes',
+    author='Reza Kabiri',
+    license='',
+)
